1b0096d622f13beba5f172ea952d328e7c3ed8fd
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-10-24 10:58:18 +0300

    Added lang change

---
+++ js/lang.js
@@ -0,0 +1,102 @@
+const langArr = {
+    "unit": {
+        "ru": "Игорный Дом",
+        "en": "House of Gambling",
+    },
+    "about": {
+        "ru": "О нас",
+        "en": "About us",
+    },
+    "brands": {
+        "ru": "Бренды",
+        "en": "Brands",
+    },
+    "comissions": {
+        "ru": "Комиссии",
+        "en": "Comissions",
+    },
+    "news": {
+        "ru": "Новости",
+        "en": "News",
+    },
+    "contact": {
+        "ru": "Контакты",
+        "en": "Contact us",
+    },
+    "careers": {
+        "ru": "Карьера",
+        "en": "Careers",
+    },
+    "log": {
+        "ru": "Войти",
+        "en": "Log in",
+    },
+    "sign": {
+        "ru": "Подписка",
+        "en": "Sign up",
+    },
+    "house": {
+        "ru": "ИГОРНЫЙ",
+        "en": "HOUSE OF",
+    },
+    "colored": {
+        "ru": "ДОМ",
+        "en": "GAMBLING",
+    },
+    "raise": {
+        "ru": "Повышай свой ROI с помощью прямого рекламодателя",
+        "en": "Raise your ROI with direct advertiser",
+    },
+    "partner": {
+        "ru": "Стать партнером",
+        "en": "Become a Partner",
+    },
+    "blog": {
+        "ru": "БЛОГ",
+        "en": "BLOG",
+    },
+    "post-1": {
+        "ru": "Способы оплаты: Skrill, Neteller, webmoney, банковский перевод",
+        "en": "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+    },
+    "post-2": {
+        "ru": "Способы оплаты: Skrill, Neteller, webmoney, банковский перевод",
+        "en": "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+    },
+    "post-3": {
+        "ru": "Способы оплаты: Skrill, Neteller, webmoney, банковский перевод",
+        "en": "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+    },
+    "post-4": {
+        "ru": "Способы оплаты: Skrill, Neteller, webmoney, банковский перевод",
+        "en": "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+    },
+    "post-5": {
+        "ru": "Способы оплаты: Skrill, Neteller, webmoney, банковский перевод",
+        "en": "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+    },
+    "terms": {
+        "ru": "Условия и Положения",
+        "en": "Terms & Conditions",
+    },
+    "cookies": {
+        "ru": "Файлы cookies",
+        "en": "cookies",
+    },
+    "contacts": {
+        "ru": "Контакты",
+        "en": "contacts",
+    },
+    "careers-2": {
+        "ru": "Карьера",
+        "en": "Careers",
+    },
+    "guide": {
+        "ru": "Руководство по бренду",
+        "en": "brand guide",
+    },
+    "social": {
+        "ru": "Мы в социальных сетях:",
+        "en": "Our social media:",
+    }
+};
--- js/main.js
+++ js/main.js
@@ -1,23 +1,54 @@
+//Смена языка
+
+const allLang = ['en', 'ru'];
+var lang = $('.header__btn-lang-text').attr('value');
+const btnLang = document.querySelector('.header__btn-lang');
+
+location.href = window.location.pathname + '#' + lang;
+//Функция перенаправить на url с указанием языка 
+function changeURLLanguage() {
+    location.href = window.location.pathname + '#' + lang;
+}
+
+// Функция перевода элементов
+function changeLanguage() {
+    if (!allLang.includes(lang)) {
+        location.href = window.location.pathname + '#en';
+    }
+
+    document.querySelector('title').innerHTML = langArr["unit"][lang];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lang-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][lang];
+        }
+    }
+}
 
 $(function () {
-    // Смена языка на кнопке
+    // Смена языка
     $('.header__btn-lang').on('click', function () {
+        // смена языка на кнопке
         $('.header__btn-lang-text').html() == "EN" ? $('.header__btn-lang-text').html('RU') : $('.header__btn-lang-text').html('EN');
+        // смена языка в переменной
+        lang == "en" ? lang = "ru" : lang = "en";
+        // вызов функции смены URL 
+        changeURLLanguage();
+        // вызов функции перевода элементов
+        changeLanguage();
     });
+
     // Открытие и закрытие мобильного меню
     $('.header__burger').on('click', function () {
         $('.header__burger, .header__menu').toggleClass('active');
         $('.header__wide-btns').toggleClass('active');
         $('.body').toggleClass('lock');
     });
+
     // Смена порядка линий из фишек после поворота из вертикали в горизонталь
     if ($(window).width() < 1599) {
         $('.content__tokens-line-three').insertBefore('.content__tokens-line-one');
         $('.content__tokens-line-two').insertBefore('.content__tokens-line-one');
     }
-    // if ($(window).width() < 766) {
-    //     let buttons = document.getElementById("wide-btns");
-    //     $('.header__menu').append(buttons);
-    // }
 });
 
